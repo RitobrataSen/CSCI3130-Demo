d64ba2a39dc1790c4399b948070baa8cc5f2b8ad
RitobrataSen <rt648981@dal.ca>
2018-05-17 12:56:15 -0300

    Added methods
    Methods include button functionality and the references to the different objects on the screen

--- app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
+++ app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.acme.csci3130_demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button bt = (Button) findViewById(R.id.bt);
+        bt.setOnClickListener(vo);
+
     }
+
+    View.OnClickListener vo = new View.OnClickListener() {
+
+        @Override
+        public void onClick(View view) {
+
+
+            EditText et = (EditText) findViewById(R.id.editText);
+            TextView tv = (TextView) findViewById(R.id.textView);
+
+            tv.setText(et.getText().toString());
+        }
+    };
+
 }
